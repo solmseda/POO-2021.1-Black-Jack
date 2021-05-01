74233da9cc7ab7ac95e014795f92f1ad54c1e37e
raks <raquelolho@hotmail.com>
2021-05-01 13:03:34 -0300

    funcoes do bank e exdend do dealer

--- src/Bank.java
+++ src/Bank.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Bank {
 	public int Quant_100;
 	public int Quant_50;
@@ -6,9 +6,22 @@ public class Bank {
 	public int Quant_10;
 	public int Quant_5;
 	
-	//Vai entregar uma quantidade de dinheiro
-
-	public int give_money() {
-		return 0;
+	//Vai entregar uma quantidade de dinheiro baseada em uma quantidade especifica de cada tipo de moeda
+	
+	
+	public List<Integer> give_money(int Quant_100,int Quant_50, int Quant_20, int Quant_10,int Quant_5) {
+		List<Integer> allcoins= new ArrayList<Integer>();
+		allcoins.add(Quant_100);
+		allcoins.add(Quant_50);
+		allcoins.add(Quant_20);
+		allcoins.add(Quant_10);
+		allcoins.add(Quant_5);
+		return allcoins;
+		}
+	
+	//De acordo com as regras, sao 2 moedas de 100 e de 50, 5 de 20 e de 10 e 8 de 5
+	public List<Integer> GiveInitialMoney() {
+		List<Integer> initialMoney= give_money(2,2,5,5,8);
+		return initialMoney;
 		}
 	}
--- src/Dealer.java
+++ src/Dealer.java
@@ -1,5 +1,5 @@
 import java.util.List;
-public class Dealer {
+public class Dealer extends Player{
 	List<Card>hand;
 	
 	public void give_card(Player player) {
