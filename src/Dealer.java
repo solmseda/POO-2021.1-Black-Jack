6e4c5c098af2be1a1f7b1b6ff6aa554078d9d9d6
raks <raquelolho@hotmail.com>
2021-05-02 18:23:45 -0300

    tirando hand de dealer

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,8 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 public class Dealer extends Player{
-	List<Card>hand = new ArrayList<Card>();
-
+	
 	public Card GiveCard(Deck deck) {
 		Card card = deck.RemoveCard();
 		return card;
